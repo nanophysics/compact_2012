7b3494dd23594c22ab895a4ad8c39c4f83c0ceb6
peter.maerki <git.peter@ergoinfo.ch>
2020-01-04 22:35:01 +0100

    .

--- Drivers/compact_2012/calibration_correction/20190606_02/calibration_correction_correction_dac_12_plot.py
+++ Drivers/compact_2012/calibration_correction/20190606_02/calibration_correction_correction_dac_12_plot.py
@@ -9,16 +9,15 @@ numpy_file = np.load('calibration_correction.npz')
 
 data = numpy_file['data']
 shape = data.shape
-#print(data)
 channel = 0
 correction_dac_12 = data[channel]
 length = correction_dac_12.size
 
 for channel in range(shape[0]):
     plt.plot(data[channel])
-    print(channel)
+    #print(channel)
 
-print (length)
+#print (length)
 plt.plot(np.ones_like(correction_dac_12) * 2**12)
 plt.plot(np.ones_like(correction_dac_12) * 0.0)
 
